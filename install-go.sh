b3d2d93afd3cac7775ece23dd4b02568b5f50a5c
Matt <mattddowney@gmail.com>
2018-02-21 23:59:07 -0700

    pass shellcheck

--- install-go.sh
+++ install-go.sh
@@ -11,8 +11,8 @@ installed_version=$($INSTALL_LOCATION/go/bin/go version | cut -d' ' -f3)
 download_link=$(curl -s https://golang.org/dl/ | grep 'class="download downloadBox"' | grep $PLATFORM | cut -d'"' -f4)
 
 # parse the link
-download_file=$(echo $download_link | cut -d'/' -f5)
-download_version=$(echo $download_file | rev | cut -d'.' -f4- | rev)
+download_file=$(echo "$download_link" | cut -d'/' -f5)
+download_version=$(echo "$download_file" | rev | cut -d'.' -f4- | rev)
 
 # if current version is the latest, don't install
 if [ "$installed_version" == "$download_version" ]
@@ -22,10 +22,10 @@ then
 fi
 
 # download the release
-wget $download_link
+wget "$download_link"
 
 # extract the archive to the install location
-tar xzf $download_file -C $INSTALL_LOCATION
+tar xzf "$download_file" -C $INSTALL_LOCATION
 
 # create symlinks
 ln -s "$INSTALL_LOCATION/go/bin/go" "$SYMLINK_LOCATION/go"
@@ -33,4 +33,4 @@ ln -s "$INSTALL_LOCATION/go/bin/gofmt" "$SYMLINK_LOCATION/gofmt"
 ln -s "$INSTALL_LOCATION/go/bin/godoc" "$SYMLINK_LOCATION/godoc"
 
 # remove the archive
-rm $download_file
+rm "$download_file"
