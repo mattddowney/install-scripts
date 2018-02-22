6cefb096d95d4aad2feaab0358c99fb191da9e10
Matt <mattddowney@gmail.com>
2018-02-22 02:15:05 -0700

    get latest release from github api

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dockerComposeVersion=$1
+DOWNLOAD_PACKAGE_NAME="docker-compose-Linux-x86_64"
 
 # ensure user is root
 if [ "$EUID" -ne 0 ]
@@ -8,12 +8,25 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-# ensure a version number is passed in
-if [ -z "$dockerComposeVersion" ]
+# get the latest release info from the github API
+api_output=$(curl https://api.github.com/repos/docker/compose/releases/latest)
+
+# get the currently installed version
+installed_version=$(docker-compose -v | cut -d' ' -f3 | cut -d',' -f1)
+
+# get the latest version from the api output
+latest_version=$(echo "$api_output" | jq -r .name)
+
+# if current version is the latest, don't install
+if [ "$installed_version" == "$latest_version" ]
 then
-    echo "Usage: $0 <docker_compose_version_number>"
-    exit 1
+    echo "$installed_version already the latest version"
+    exit
 fi
 
-curl -L "https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+# get the download link from the api output
+download_url=$(echo "$api_output" | jq -r ".assets[] | select(.name == \"$DOWNLOAD_PACKAGE_NAME\") | .browser_download_url")
+
+# download and install
+curl -L "$download_url" > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
