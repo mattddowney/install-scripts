91ff2b514b67b955f19cb5e97d8341d5ee219b3c
mattddowney <mattddowney@gmail.com>
2021-10-10 02:19:32 -0600

    add portainer install script

---
+++ install-portainer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOCKER_IMAGE="portainer/portainer-ce:latest"
+
+# ensure user is root
+if [ "$EUID" -ne 0 ]
+  then echo "Must be run as root. Aborting."
+  exit 1
+fi
+
+# ensure docker is installed
+which docker > /dev/null
+if [ "$?" -ne 0 ]
+  then echo "Docker not installed. Aborting."
+  exit 2
+fi
+
+docker pull "$DOCKER_IMAGE"
+docker run \
+  --name=portainer \
+  --restart=always \
+  --volume=/var/run/docker.sock:/var/run/docker.sock \
+  --volume=portainer_data:/data \
+  --publish=9000:9000 \
+  --detach \
+  portainer/portainer-ce
