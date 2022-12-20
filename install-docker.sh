bbc82c2364c1fc45d01cbb7658de138530657167
Matthew Downey <mattddowney@gmail.com>
2022-12-20 13:43:30 -0700

    update to new instructions on docker.com
    Base installation off of instructions here:
    https://docs.docker.com/engine/install/ubuntu/

--- install-docker.sh
+++ install-docker.sh
@@ -9,16 +9,16 @@ fi
 
 # install required packages
 apt-get install -y \
-    apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    gnupg \
+    lsb-release
 
 # add the docker repo key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # add the docker repository
-add-apt-repository \
+add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
@@ -27,4 +27,4 @@ add-apt-repository \
 apt-get update
 
 # install the latest version of docker
-apt-get install -y docker-ce
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
